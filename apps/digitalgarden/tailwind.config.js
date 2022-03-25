d31ce1a11b6b844caae90feec1354cbc5a489024
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-03-25 11:54:13 -0600

    set up tailwind purging

--- apps/digitalgarden/tailwind.config.js
+++ apps/digitalgarden/tailwind.config.js
@@ -1,7 +1,13 @@
+const { join } = require('path');
+
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind')
+
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: [join(__dirname, 'pages/**/*.{js, ts, jsx, tsx}'),
+...createGlobPatternsForDependencies(__dirname)]
 }
