a7080c9f9070854f7d0daa58bf1bf890ef75596f
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-03-25 12:14:24 -0600

    set up a global tailwind config and add the typography plugin

--- apps/digitalgarden/tailwind.config.js
+++ apps/digitalgarden/tailwind.config.js
@@ -1,6 +1,6 @@
 const { join } = require('path');
 
-const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind')
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 module.exports = {
   content: [],
@@ -8,6 +8,9 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  purge: [join(__dirname, 'pages/**/*.{js, ts, jsx, tsx}'),
-...createGlobPatternsForDependencies(__dirname)]
-}
+  purge: [
+    join(__dirname, 'pages/**/*.{js, ts, jsx, tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  presets: [require('../../tailwind-workspace-preset.js')],
+};
