dbc6be231f980b96ab3ba7e64980460e790c7bd9
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-04-01 11:52:16 -0600

    created an article generator with prompts

---
+++ tools/generators/new-article/index.ts
@@ -0,0 +1,27 @@
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  generateFiles,
+  joinPathFragments,
+  names,
+} from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+
+interface NewArticlesSchemaOptions {
+  title: string;
+  author: string;
+  excerpt?: string;
+}
+
+export default async function (tree: Tree, schema: NewArticlesSchemaOptions) {
+  generateFiles(tree, joinPathFragments(__dirname, './files'), './_articles', {
+    title: schema.title,
+    author: schema.author,
+    excerpt: schema.excerpt || '',
+    normalizedTitle: names(schema.title).fileName,
+    creationDate: new Date().toISOString(),
+  });
+
+  await formatFiles(tree);
+}
